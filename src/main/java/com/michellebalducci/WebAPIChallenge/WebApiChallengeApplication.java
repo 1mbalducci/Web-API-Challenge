3b5a068677da39f800e3c2f8b5a57cebf2b1daa4
Michelle Balducci <michelle.balducci@gmail.com>
2023-08-31 13:24:06 -0500

    starting

---
+++ src/main/java/com/michellebalducci/WebAPIChallenge/WebApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.michellebalducci.WebAPIChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michellebalducci/WebAPIChallenge/WebApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michellebalducci.WebAPIChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
