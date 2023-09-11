19b7572b9a107201fb653d165cb09841565b60d2
Michelle Balducci <michelle.balducci@gmail.com>
2023-09-11 12:10:20 -0500

    controller

--- src/main/java/com/michellebalducci/WebAPIChallenge/controller/CustomerRewardsPointsController.java
+++ src/main/java/com/michellebalducci/WebAPIChallenge/controller/CustomerRewardsPointsController.java
@@ -1,14 +1,41 @@
 package com.michellebalducci.WebAPIChallenge.controller;
 
+import com.michellebalducci.WebAPIChallenge.entity.Order;
 import com.michellebalducci.WebAPIChallenge.repository.CustomerRepository;
 import com.michellebalducci.WebAPIChallenge.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200/", allowCredentials = "true")
+@RequestMapping
 public class CustomerRewardsPointsController {
-    private final CustomerRepository customerRepository;
-    private final OrderRepository orderRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    OrderRepository orderRepository;
+
+    public CustomerRewardsPointsController(CustomerRepository customerRepository, OrderRepository orderRepository) {
+        this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/rewards/allCustomers")
+    public ResponseEntity<List<Order>> loadRewardsAllCustomers() {
+        List<Order> listOrders = orderRepository.findAll();
+        Order order = new Order();
 
 
-@Get{/id}
+        return ResponseEntity.status(HttpStatus.OK).body(listOrders);
+
+    }
 }
